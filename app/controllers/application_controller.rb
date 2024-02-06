e21e9f50e3a6f5f45669cfe34ebb4089d8f484b0
Mahdi Noori <mahdinoori2389@gmail.com>
2024-02-06 16:34:27 +0430

    Add authentication in backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
-end
+    attr_reader :current_user
+  
+    before_action :authenticate_token!
+  
+    private
+  
+    def authenticate_token!
+      header = request.headers['Authorization']
+      header = header.split.last if header
+      begin
+        @payload = JsonWebToken.decode(header)
+        @current_user = User.find(@payload['sub'])
+      rescue JWT::ExpiredSignature
+        render json: { error: 'your authentication token is expired' }
+      rescue JWT::DecodeError
+        render json: { error: 'Incorrect authentication token' }
+      end
+    end
+  end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,26 @@
+class RegistrationsController < ApplicationController
+  skip_before_action :authenticate_token!
+
+  def create
+    if user_exists?(sign_up_params[:email])
+      render json: { error: 'Email already exists. Please choose a different email!' }, status: :unprocessable_entity
+    else
+      user = User.new(sign_up_params)
+      if user.save
+        render json: { user: UserSerializer.new(user), token: JsonWebToken.encode({ sub: user.id }) }
+      else
+        render json: { error: "'Failed to create user. Check on input!" }, status: :unprocessable_entity
+      end
+    end
+  end
+
+  protected
+
+  def user_exists?(email)
+    User.exists?(email:)
+  end
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
