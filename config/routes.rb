10b15e57e1c345be012d65a2a9b9d5b7da25787a
Ismail mayito <ismayito1@gmail.com>
2024-02-06 17:54:35 +0300

    Added column reason to appointments table

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :users
-      resources :doctors
-      resources :appointments
+      resources :doctors ,only: [:index,:show]  do
+      resources :appointments ,only: [:index,:create,:destroy] 
+      end
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_05_115653) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_134929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_05_115653) do
     t.bigint "doctor_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "reason"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
