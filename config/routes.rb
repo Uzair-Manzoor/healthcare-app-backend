0fee756ec993965adbb28195cd137e1c999b2689
Ismail mayito <ismayito1@gmail.com>
2024-02-06 18:01:48 +0300

    Add create doctor Api route

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :users
-      resources :doctors ,only: [:index,:show]  do
+      resources :doctors ,only: [:index,:show,:create]  do
       resources :appointments ,only: [:index,:create,:destroy] 
       end
     end
