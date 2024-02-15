8b63e63d93026dfc6b83056f53fd8011d7b6fc71
Ismail mayito <ismayito1@gmail.com>
2024-02-15 18:23:30 +0300

    fix assets compilation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,9 @@ Rails.application.configure do
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
+  
+  # This line tells Rails not to compile assets in production, which is unnecessary for API-only applications.
+  config.assets.compile = false
 
 
   # Raises error for missing translations.
